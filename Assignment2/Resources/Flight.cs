c668f881006b3c610e793f5cef77bdf9e9ba94af
nmgoddard <149209419+nmgoddard@users.noreply.github.com>
2024-04-12 23:48:59 -0600

    Content

---
+++ Assignment2/Resources/Flight.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2.Resources
+{
+	internal class Flight
+	{
+		private string flightCode;
+		public string FlightCode { get; }
+		private string airlineName;
+		public string AirlineName { get; }
+		private string originAirport;
+		public string OriginAirport { get; }
+		private string destAirport;
+		public string DestAirport { get; }
+		private double cost;
+		public double Cost { get; }
+		private string weekday;
+		public string Weekday { get; }
+		private string time;
+		public string Time { get; }
+		public bool isSelected = false; 
+
+		public Flight() {}
+		public Flight(string flightCode, string airlineName, string originAirport, string destAirport, string weekday, string time, double cost) 
+		{
+			this.flightCode = flightCode;
+			this.airlineName = airlineName;
+			this.originAirport = originAirport;
+			this.destAirport = destAirport;
+			this.cost = cost;
+			this.time = time;
+			this.weekday = weekday;
+		}
+
+		public override string ToString() 
+		{
+			return $"{flightCode} - {airlineName}- {String.Format("${0:0.00}", cost)} - {time}, {weekday}";
+		}
+	}
+}
---
+++ Assignment2/Resources/Reservation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2.Resources
+{
+	public class Reservation
+	{
+		private string reservationCode;
+		public string ReservationCode { get; }
+		private string flightCode;
+		public string FlightCode { get; }
+		private string airlineName;
+		public string AirlineName { get; }
+		private double cost;
+		public double Cost { get; }
+		private string name;
+		public string Name { get; set; }
+		private string citizenship;
+		public string Citizenship { get; set; }
+		private bool isActive;
+		public bool IsActive { get; set; }
+	}
+}
